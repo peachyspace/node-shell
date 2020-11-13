ea3960f6c2d9c26a416d82c22dcf2b7b7debcfa5
Priscila <priscilapintado66@gmail.com>
2020-11-13 18:27:23 -0500

    pwd part 1

--- bash.js
+++ bash.js
@@ -1,1 +1,14 @@
-"egrgrteer";
+process.stdout.write("prompt > ");
+process.stdin.on("data", (data) => {
+  //console.log(typeof data);
+  const cmd = data.toString().trim();
+  if (cmd === "pwd") {
+    process.stdout.write(`${process.cwd()}`);
+    process.stdout.write("\nprompt > ");
+  } else {
+    process.stdout.write(`You typed:` + cmd);
+    process.stdout.write("\nprompt > ");
+  }
+
+  //process.stdout.write("pwd: " + cmd);
+});
